6c220f72aa21608b4803d668925d02b64af1f45d
multiwebview <wz546500392@gmail.com>
2020-05-07 10:58:54 +0800

    finished all basic functions

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"strings"
 
 	"youdao_alfred/alfred"
 	"youdao_alfred/youdao"
@@ -12,10 +13,19 @@ func main() {
 	key := flag.String("k", "Hello World", "the sentence you want to translate")
 	flag.Parse()
 
-	result := alfred.NewResult()
+	//fmt.Println("SRC -> ", *key, " LEN -> ", len(*key))
+	//fmt.Println("OTHERS -> ", flag.Args())
+	if len(flag.Args()) > 0 {
+		*key += " " + strings.Join(flag.Args(), " ")
+	}
+	//fmt.Println("SRC -> ", *key, " LEN -> ", len(*key))
 
+	result := alfred.NewResult()
 	dst := youdao.Translate(*key)
-	result.Append(alfred.NewItem(dst, "test subtitle", dst))
+	result.Append(alfred.NewItem(dst, *key, dst))
+
+	//fmt.Println("DST -> ", dst)
+	//fmt.Println("result -> ", result)
 
 	result.End()
 }
